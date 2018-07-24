da0b352f7eeb8e2c7a65105f4acf5d6288baae94
joao vitor <joaovitormalvestio@gmail.com>
2018-07-24 00:20:56 -0300

    Ïncluido usuario medico

--- src/clinicamedicapoo/medico/Medico.java
+++ src/clinicamedicapoo/medico/Medico.java
@@ -9,13 +9,18 @@ import clinicamedicapoo.utilitarios.Pessoa;
 import clinicamedicapoo.utilitarios.Sexo;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author miche
  */
 @Entity
-public class Medico extends Pessoa {
+public class Medico extends Pessoa {    
+    public static EntityManagerFactory factory = Persistence.createEntityManagerFactory("ClinicaMedicaPOO");
+    public static EntityManager manager = factory.createEntityManager();
 
     public Medico(String nome, String sobrenome, String cpf, String rg, Sexo sexo, String dataNascimento, String rua, String numero, String bairro, String cep, String cidade, String estado, String telefone_residencial, String telefone_celular, String email) {
         super(nome, sobrenome, cpf, rg, sexo, dataNascimento, rua, numero, bairro, cep, cidade, estado, telefone_residencial, telefone_celular, email);
@@ -26,6 +31,20 @@ public class Medico extends Pessoa {
 
     public Medico(String nome) {
         super(nome);
+    }
+    
+    public Medico inserirMedico(String nome, String sobrenome, String cpf, String rg, Sexo sexo, String dataNascimento, String rua, String numero, String bairro, String cep, String telefone_residencial, String email, String cidade, String estado){
+        Medico medico = new Medico(nome,sobrenome,cpf,rg,sexo,dataNascimento,rua,numero,bairro,cep,cidade,estado,telefone_residencial,telefone_celular,email);
+        try{
+           manager.getTransaction().begin();
+           manager.persist(medico);
+           manager.getTransaction().commit();
+           return medico;
+        }
+        catch(Exception e){
+            System.out.println("Erro: " + e.getMessage());
+            return null;
+        }
     }
     
 }
